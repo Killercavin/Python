a0e50f22a001128e3ddbbbd4597b12cb82ade9da
Killercavin <cavinlarry001@gmail.com>
2023-06-11 21:17:14 +0300

    Updated some files

---
+++ names.py
@@ -0,0 +1,5 @@
+from name_ import get_formatted_name
+def full_name():
+    formatted_name = get_formatted_name('john', 'doe')
+    print(f"\tNeatly formatted name: {formatted_name}.") 
+full_name()
